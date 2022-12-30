6d8f8d672908783c4549465b3e2f7cbe2fc1b112
Oleksandr Kaleniuk <akalenuk@gmail.com>
2022-12-30 20:14:05 +0200

    Update stat_utils.erl

--- site/src/stat_utils.erl
+++ site/src/stat_utils.erl
@@ -1,4 +1,4 @@
-%   Copyright 2013 Alexandr Kalenuk (akalenuk@gmail.com)
+%   Copyright 2022 Olexandr Kalenuk (akalenuk@gmail.com)
 %
 %   Licensed under the Apache License, Version 2.0 (the "License");
 %   you may not use this file except in compliance with the License.
